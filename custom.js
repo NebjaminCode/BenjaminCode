8b53931aa89b75e9e79d801698f971cc81a2e090
Benjamin Code <bjamescode@gmail.com>
2020-07-27 13:48:42 -0700

    added slider using slick.js to projects section, have not added content or styled

---
+++ custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.project_caro').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: true,
+        mobileFirst: true,
+    });
+  });
